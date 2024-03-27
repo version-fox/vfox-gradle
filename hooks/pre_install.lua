6e9aa54db610bc30de5eb785d39d4a620026645c
ahai <1149956305@qq.com>
2024-03-27 15:02:50 +0800

    Add mirroring option

--- hooks/pre_install.lua
+++ hooks/pre_install.lua
@@ -1,29 +1,46 @@
 local util = require("util")
+local os = require("os")
 --- Returns some pre-installed information, such as version number, download address, local files, etc.
 --- If checksum is provided, vfox will automatically check it for you.
 --- @param ctx table
 --- @field ctx.version string User-input version
 --- @return table Version information
 function PLUGIN:PreInstall(ctx)
+
+    local result = {}
     local version = ctx.version
-    local downloadUrl = util.DownloadInfoUrl:format(version)
+    result.version = version
+
+    local downloadUrl = ""
+    local imageUrl = os.getenv("GRADLE_DOWNLOAD_IMAGE")
+
+    if imageUrl ~=nil then
+        downloadUrl = imageUrl.."/gradle-"..version.."-bin.zip"
+    else
+        downloadUrl = util.DownloadInfoUrl:format(version)
+    end
+
+    result.url = downloadUrl
 
     local resp, err = http.get({
         url = downloadUrl..".sha256"
     })
-    if err ~= nil then
-        error(err)
+
+    if err then
+        error("HTTP request error: " .. err)
     end
 
-    if resp.status_code ~= 200 then
+    if resp.status_code == 200 then
+        if resp.body then
+            result.sha256 = resp.body
+        else
+            error("Empty body in HTTP response")
+        end
+    elseif resp.status_code == 404 then
+        print("This version does not have checksums")
+    else
         return nil
     end
 
-    local sha256 = resp.body
-
-    return {
-        version = version,
-        sha256 = sha256,
-        url = downloadUrl,
-    }
+    return result
 end
